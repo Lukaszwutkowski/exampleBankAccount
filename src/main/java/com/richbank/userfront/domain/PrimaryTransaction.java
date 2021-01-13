20899183d42e73957ebc3652d10831eeade9a492
Lukaszwutkowski <64429535+Lukaszwutkowski@users.noreply.github.com>
2021-01-13 10:22:36 +0100

    added hibernate functionallity

--- src/main/java/com/richbank/userfront/domain/PrimaryTransaction.java
+++ src/main/java/com/richbank/userfront/domain/PrimaryTransaction.java
@@ -1,9 +1,14 @@
 package com.richbank.userfront.domain;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Entity
 public class PrimaryTransaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date date;
     private String description;
@@ -11,7 +16,6 @@ public class PrimaryTransaction {
     private String status;
     private double amount;
     private BigDecimal availableBalance;
-    private PrimaryAccount primaryAccount;
 
     public PrimaryTransaction() {}
 
@@ -24,6 +28,10 @@ public class PrimaryTransaction {
         this.availableBalance = availableBalance;
         this.primaryAccount = primaryAccount;
     }
+
+    @ManyToOne
+    @JoinColumn(name = "primary_account_id")
+    private PrimaryAccount primaryAccount;
 
     public Long getId() {
         return id;
