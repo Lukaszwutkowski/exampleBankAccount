0c5529285718cc1e008625d09b2acc6f253c4f80
Lukaszwutkowski <64429535+Lukaszwutkowski@users.noreply.github.com>
2021-01-18 10:37:33 +0100

    added userFront.html template

--- src/main/java/com/richbank/userfront/controller/HomeController.java
+++ src/main/java/com/richbank/userfront/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.richbank.userfront.controller;
 
 import com.richbank.userfront.dao.RoleDao;
+import com.richbank.userfront.domain.PrimaryAccount;
+import com.richbank.userfront.domain.SavingsAccount;
 import com.richbank.userfront.domain.User;
 import com.richbank.userfront.domain.security.UserRole;
 import com.richbank.userfront.service.UserService;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.security.Principal;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -64,5 +67,17 @@ public class HomeController {
 
             return "redirect:/";
         }
+    }
+
+    @RequestMapping("/userFront")
+    public String userFront(Principal principal, Model model) {
+        User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+        SavingsAccount savingsAccount = user.getSavingsAccount();
+
+        model.addAttribute("primaryAccount", primaryAccount);
+        model.addAttribute("savingsAccount", savingsAccount);
+
+        return "userFront";
     }
 }
